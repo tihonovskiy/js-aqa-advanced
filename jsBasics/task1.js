6cb35444a5107d5812c993cc0365988f26967df9
tykhonovskyi <tihonovskiy@gmail.com>
2025-10-11 19:52:38 +0200

    Homework 4.1 JavaScript primitive data types

---
+++ jsBasics/task1.js
@@ -0,0 +1,16 @@
+var helloString;
+helloString = "Hello";
+
+const NUMBER_100 = 100;
+
+let boolTrue = true;
+
+var myNull = null;
+
+let myUndefined;
+
+console.log("String : ", helloString);
+console.log("Number : ", NUMBER_100);
+console.log("Boolean : ", boolTrue);
+console.log("Null : ", myNull);
+console.log("Undefined : ", myUndefined);
---
+++ jsBasics/task3.js
@@ -0,0 +1,11 @@
+let personsAge17 = 17;
+let personsAge18 = 18;
+let personsAge30 = 30;
+
+let personsAge17isAdult = personsAge17 >= 18;
+let personsAge18isAdult = personsAge18 >= 18;
+let personsAge30isAdult = personsAge30 >= 18;
+
+console.log(personsAge17isAdult);
+console.log(personsAge18isAdult);
+console.log(personsAge30isAdult);
