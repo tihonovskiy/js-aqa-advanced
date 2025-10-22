6fc33a2883cf31183780ea95fd94f0dadc764ad0
tykhonovskyi <tihonovskiy@gmail.com>
2025-10-22 21:22:36 +0200

    add to Fixed for result

--- jsBasics/task4.js
+++ jsBasics/task4.js
@@ -1,16 +1,16 @@
 //area of ​​a circle
 let circleRadius = 5;
-let circleArea = Math.PI * Math.pow(circleRadius, 2);
+let circleArea = (Math.PI * Math.pow(circleRadius, 2)).toFixed(2);
 console.log("circleArea = ", circleArea);
 
 //area of ​​a rectangle
 const LENGHT = 5;
 const WIDTH = 7;
-let rectangleArea = LENGHT * WIDTH
+let rectangleArea = (LENGHT * WIDTH).toFixed(2);
 console.log("rectangleArea = ", rectangleArea);
 
 //cylinder volume
 let cylinderRadius = 10;
 let cylinderHeight = 15;
-let cylinderVolume = Math.PI * Math.pow(cylinderRadius, 2) * cylinderHeight;
+let cylinderVolume = (Math.PI * Math.pow(cylinderRadius, 2) * cylinderHeight).toFixed(2);
 console.log("cylinderVolume = ", cylinderVolume);
