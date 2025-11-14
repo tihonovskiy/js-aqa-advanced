6a969cc46d527d2fd3792a1b9d8ae7897d3d2345
tykhonovskyi <tihonovskiy@gmail.com>
2025-11-14 10:40:49 +0100

    HW9 Objects in JavaScript

---
+++ homeworks/homework9/task1.js
@@ -0,0 +1,8 @@
+const student = {};
+
+student.firstName = "Іван";
+student.lastName = "Петренко";
+student.age = 20;
+student.courses = ["Математика", "Історія", "Програмування"];
+
+console.log(student);
---
+++ homeworks/homework9/task2.js
@@ -0,0 +1,10 @@
+const book ={
+    title: "Мауглі",
+    author: "Редьярд Кіплінг",
+    year: "Ім'я власника автомобіля"
+}
+
+const {title, author} = book;
+
+console.log("Назва книги:", title);
+console.log("Автор книго:", author);
